480d51f3dcdb651cd3d447f46dde0fd50debac96
shashikant-kesharwani <rajjukesharwani2002@gmail.com>
2025-10-06 11:11:59 +0530

    add new feature

--- customer.py
+++ customer.py
@@ -15,10 +15,16 @@ try:
 
         def createuser(self):
             self.temp = self.db.db_query(f"select status from customers where username = '{self.__username}' and phone = '{self.__phone}' and account_number = '{self.__account_number}';")
-            if self.temp[0][0] == 1:
-                print("This Person is Already Registerd In This Bank.")
-                mobj = Main()
-                mobj.main()
+            if self.temp == [] or self.temp[0][0] == 1:
+                if self.temp == []:
+                    self.db.db_query(
+                        f"insert into customers values ('{self.__username}','{self.__password}','{self.__name}','{self.__age}','{self.__city}','{self.__phone}','{self.__account_number}',0,1);")
+                    self.db.mydb.commit()
+                    print(f"Your Account Create Successfully {self.__username} .")
+                elif self.temp[0][0] == 1:
+                    print("This Record Already Exists")
+                    mobj = Main()
+                    mobj.main()
             else:
                 self.db.db_query(f"insert into customers values ('{self.__username}','{self.__password}','{self.__name}','{self.__age}','{self.__city}','{self.__phone}','{self.__account_number}',0,1);")
                 self.db.mydb.commit()
